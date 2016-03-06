c143b6ba504e07648212ae4fb77fe63543e0908d
vxtkz <vxtkz.vxtkz@gmail.com>
2016-03-06 14:51:48 -0500

    .

---
+++ demo/choose.js
@@ -0,0 +1,23 @@
+var fromPlaces = ["Chicago", "New York", "Shanghai", "Hangzhou"];
+
+function chooseFrom() {
+	mountChoosePane("from");
+}
+
+function chooseTo() {
+	mountChoosePane("to");
+}
+
+function chooseDate() {
+	mountChoosePane("date");
+}
+
+function mountChoosePane(type) {
+	if(type == "from") {
+		;
+	} else if (type == "to") {
+		;
+	} else if (type == "date") {
+		;
+	}
+}
